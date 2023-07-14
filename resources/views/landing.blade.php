eb044bb5f387774a531c26dd46a2903dae41ed8b
piopi01 <vionasephenia@gmail.com>
2023-07-14 22:03:15 +0700

    first commit

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]>
+<!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+
+    <!-- Basic Page Needs
+ ================================================== -->
+    <meta charset="utf-8">
+    <title>Diagnosa Depresi</title>
+    <meta name="description" content="Professional Creative Template" />
+    <meta name="author" content="IG Design">
+    <meta name="keywords"
+        content="ig design, website, design, html5, css3, jquery, creative, clean, animated, portfolio, blog, one-page, multi-page, corporate, business," />
+    <meta property="og:title" content="Professional Creative Template" />
+    <meta property="og:type" content="website" />
+    {{-- <meta property="og:url" content="" /> --}}
+    {{-- <meta property="og:image" content="" /> --}}
+    <meta property="og:image:width" content="470" />
+    <meta property="og:image:height" content="246" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:description" content="Professional Creative Template" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="https://twitter.com/IvanGrozdic" />
+    <meta name="twitter:domain" content="http://ivang-design.com/" />
+    {{-- <meta name="twitter:title" content="" /> --}}
+    {{-- <meta name="twitter:description" content="Professional Creative Template" /> --}}
+    {{-- <meta name="twitter:image" content="http://ivang-design.com/" /> --}}
+
+    <!-- Mobile Specific Metas
+ ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="theme-color" content="#212121" />
+    <meta name="msapplication-navbutton-color" content="#212121" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="#212121" />
+
+    <!-- Web Fonts
+ ================================================== -->
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900"
+        rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+
+    <!-- CSS
+ ================================================== -->
+    <link rel="stylesheet" href="landing/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="landing/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="landing/css/style.css" />
+    <link rel="stylesheet" href="landing/css/colors/color.css" />
+    <link rel="stylesheet" href="landing/css/ionicons.min.css" />
+    <link rel="stylesheet" href="landing/css/owl.carousel.css" />
+    <link rel="stylesheet" href="landing/css/owl.transitions.css" />
+    <link rel="stylesheet" href="landing/css/footer.css">
+
+
+    <!-- Favicons
+ ================================================== -->
+    <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
+
+
+</head>
+
+<body class="royal_preloader">
+
+
+    <!-- Nav and Logo
+ ================================================== -->
+
+
+    <!-- MENU
+    ================================================== -->
+    
+        <header>
+            <nav id="menu-wrap" class="menu-back cbp-af-header">
+                <div class="menu">
+                    <a href="/">
+                        <div class="logo"></div>
+                    </a>
+                    <ul>
+                        <li>
+                            <a class="shadow-hover" href="#faq">FAQ</a>
+                        </li>
+                        <li>
+                            <a class="shadow-hover" href="#kontak">Kontak</a>
+                        </li>
+                        <li>
+                            <a href="/form-faq"  class="shadow-hover">Cek Depresi</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        
+        </header>
+
+    
+    <!-- Primary Page Layout
+ ================================================== -->
+
+    <main>
+
+        <!-- Hero Section -->
+
+        <div class="section full-height mob-height">
+            <div class="background-parallax" style="background-image: url('landing/img/full-2.jpg')"
+                data-enllax-ratio=".5" data-enllax-type="background" data-enllax-direction="vertical"></div>
+            <div class="hero-center-text-wrap">
+                <div class="container text-left">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <br>
+                            <br>
+                            <h1 class="parallax-fade-top-2 typed">Cek Tingkat <strong>Depresimu</strong>
+                                Sekarang!<br><span id="typed-1"></span></h1>
+                            <br>
+                            <style>
+                                .btn-glow:hover {
+                                  box-shadow: 0 0 10px rgba(255, 255, 255, 0.5);
+                                  transition: box-shadow 0.5s;
+                                }
+                              </style>
+                              {{-- <div style="margin-left: 12px">
+
+                                <a href="/form-faq"  class="btn btn-light btn-glow" role="button" style= "color: rgb(218, 116, 22);">
+                                  Isi form
+                                </a>
+                              </div> --}}
+                            <div style="margin-left: 12px">
+                                <a href="/form-faq" class="btn btn-light btn-glow" role="button" style="color: #EDE4FF; font-size: 30px; background-color: #1B6B93;">
+                                    Cek Depresi!!!
+                                </a>
+                            </div>
+                            
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="#top-scroll" data-gal="m_PageScroll2id">
+                <div class="scroll-to-next">ke bawah <i class="fa fa-long-arrow-down"></i></div>
+            </a>
+        </div>
+
+        <!-- Portfolio -->
+
+
+        <!-- Hero Section -->
+
+        <div class="section padding-top-bottom-big background-dark">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 text-center">
+                        <div id="owl-sep-1" class="owl-carousel owl-theme">
+                            <div class="item">
+                                <div class="quote">
+                                    <p class="lead">"DepressionTracker adalah situs yang membantu mahasiswa akhir mengukur
+                                        tingkat depresi mereka dengan mengisi formulir pertanyaan. Kami percaya bahwa
+                                        mengetahui tingkat depresi Anda adalah langkah pertama dalam menemukan solusi
+                                        dan menangani masalah ini. Selain itu, kami juga menyediakan informasi dan
+                                        sumber daya yang berguna untuk membantu Anda dalam mengelola depresi Anda."</p>
+                                    <h6>
+                                        <h6>&rarr;</h6>
+                                    </h6>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="quote">
+                                    <p class="lead">"Ingatlah bahwa DepressionTracker bukanlah pengganti layanan
+                                        profesional, jadi pastikan untuk selalu mencari bantuan medis yang tepat jika
+                                        Anda mengalami gejala depresi."</p>
+                                    <h6>&rarr;</h6>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="quote">
+                                    <p class="lead">"Kami membuat DepressionTracker sebagai proyek akhir untuk membantu
+                                        teman-teman kami yang mungkin mengalami depresi untuk mengetahui tingkat depresi
+                                        mereka dan menemukan solusi sesuai."</p>
+                                    <h6>&rarr;</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+
+        <!-- FAQ Section -->
+        <section id="faq" class="bg-light py-5">
+            <div class="container">
+                <h2 class="text-center mb-5">Pertanyaan yang Sering Diajukan - FAQ</h2>
+                <div class="accordion" id="faqAccordion">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link" type="button" data-toggle="collapse"
+                                    data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    Apa itu DepressionTracker?
+                                </button>
+                            </h2>
+                        </div>
+
+                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                            data-parent="#faqAccordion">
+                            <div class="card-body">
+                                DepressionTracker adalah sebuah situs yang membantu mahasiswa akhir mengukur tingkat depresi
+                                mereka dengan mengisi formulir pertanyaan. Kami menyediakan solusi sesuai setelah
+                                mahasiswa mengisi formulir tersebut.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingTwo">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                    data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    Siapa yang bisa mengakses DepressionTracker?
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
+                            data-parent="#faqAccordion">
+                            <div class="card-body">
+                                DepressionTracker ditujukan untuk mahasiswa akhir, namun siapa saja dapat mengakses situs ini
+                                dan mengisi formulir untuk mengetahui tingkat depresi mereka.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingThree">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                    data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    Apakah hasil dari DepressionTracker dapat diandalkan?
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                            data-parent="#faqAccordion">
+                            <div class="card-body">
+                                Hasil dari DepressionTracker adalah sebuah estimasi dari tingkat depresi seseorang, dan tidak
+                                bisa dianggap sebagai diagnosis yang pasti. Kami sangat menyarankan agar seseorang yang
+                                merasa memiliki tingkat depresi yang tinggi untuk segera meminta bantuan profesional.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingFour">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                    data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                    Bagaimana cara mengakses solusi yang ditawarkan DepressionTracker?
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseFour" class="collapse" aria-labelledby="headingFour"
+                            data-parent="#faqAccordion">
+                            <div class="card-body">
+                                Setelah mengisi formulir, mahasiswa akan menerima rekomendasi solusi sesuai dengan
+                                tingkat depresi yang terdeteksi. Kami juga menyediakan tautan ke sumber informasi dan
+                                bantuan profesional yang dapat membantu mahasiswa mengatasi depresi mereka.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </section>
+
+
+
+
+        <!-- FOOTER
+        ============================================= -->
+        <section id="kontak">
+            <footer>
+                <div class="footerContainer">
+                    <div class="socialIcons">
+                        <a href="https://www.facebook.com/viona.stmrng"><i class="fa-brands fa-facebook"></i></a>
+                        <a href="https://www.instagram.com/vionastrng_/"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="https://twitter.com/wiseconnector/status/1679760183688306688"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="https://www.google.com/"><i class="fa-brands fa-google-plus"></i></a>
+                        <a href="https://youtu.be/fWFuQR_Wt4M"><i class="fa-brands fa-youtube"></i></a>
+                    </div>
+                    <div class="footerNav">
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="https://www.youtube.com/results?search_query=news+depression">News</a></li>
+                            <li><a href="/">Tentang</a></li>
+                            <li><a href="#kontak">Kontak</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footerBotton">
+                    <p>Copyright &copy;2023 Designed by <span class="designer">Viona</span></p>
+                </div>
+            </footer>
+        </section>
+
+    </main>
+
+
+    <!-- JAVASCRIPT
+    ================================================== -->
+    <script src="landing/js/jquery.js"></script>
+    <script src="landing/js/royal_preloader.min.js"></script>
+    <script src="landing/js/popper.min.js"></script>
+    <script src="landing/js/bootstrap.min.js"></script>
+    <script src="landing/js/plugins.js"></script>
+    <script src="landing/js/custom.js"></script>
+    <script>
+        // Type text
+
+        var typed = new Typed('#typed-1', {
+            strings: ['diagnosa', 'solusi', 'atasi'],
+            typeSpeed: 45,
+            backSpeed: 0,
+            startDelay: 200,
+            backDelay: 2200,
+            loop: true,
+            loopCount: false,
+            showCursor: true,
+            cursorChar: "_",
+            attr: null
+        });
+    </script>
+    <!-- End Document
+================================================== -->
+</body>
+
+</html>
